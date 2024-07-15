7958641f84a41269e0f5a8811f45b1450c511af5
trentys <trentys@yahoo.com>
2024-07-15 15:29:54 -0700

    layout change test

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,12 @@ import Footer from './Footer';
 const Layout = ({ children }) => {
   return (
     <div className="layout">
-      <main>{children}</main>
+      <main className="content">
+        <div className="container">
+          <h1>This is a test heading in Layout</h1>
+          {children}
+        </div>
+      </main>
       <Footer />
     </div>
   );
